92764caea2821fbee53ef4889e38266a18c8e7ec
Brendan Comiskey <comiskeybrendan@gmail.com>
2023-04-01 17:01:53 -0400

    staples

---
+++ src/main/java/oreo/Main.java
@@ -0,0 +1,23 @@
+package oreo;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Time for Mateo");
+    }
+
+//    public int maxProfit(int[] prices) {
+//        //var for min
+//        int minPrice = Integer.MAX_VALUE;
+//        // var to retrun max price
+//        int maxProfit = 0;
+//        for (int i = 0; i < prices.length; i++) {
+//            if (prices[i] < minPrice) {
+//                minPrice = prices[i];
+//            } else if (prices[i] - minPrice > maxProfit) {
+//                maxProfit = prices[i] - minPrice;
+//            }
+//        }
+//        return maxProfit;
+//
+//    }
+}
